407ca7c3315750b35add11afc79e1141c8d0f92a
Juani Garay <jngaray93@gmail.com>
2024-03-11 16:20:08 -0300

    make build export types as well

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["lib"] })],
   // https://vitejs.dev/guide/build.html#library-mode
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
-    }
-  }
-})
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+  },
+});
